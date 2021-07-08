284c198d58d81ebace6b93beb3cbb43ffa4a70fe
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-07-08 12:37:49 +0100

    Add files via upload

---
+++ first_project/index.js
@@ -0,0 +1,16 @@
+// Option 2 - jQuerys smooth scroll
+/*
+$('.navbar a, section#projects a').on('click', function(e) {
+    if(this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+            scrollTop: $(hash).offset().top
+            }, 
+            800
+        );
+    }
+}); */
