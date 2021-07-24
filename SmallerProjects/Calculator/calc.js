3b6cd7ab3f432c46026a1d8f8adb25e344bb6c26
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-07-24 12:33:05 +0100

    Update calc.js

---
+++ SmallerProjects/Calculator/calc.js
@@ -0,0 +1,6 @@
+// Creating constant variables for all different buttons
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-allclear]');
+const equalsButton = document.querySelector('[data-equals]');
