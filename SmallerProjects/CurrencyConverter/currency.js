862309a3288ec5a4797f508fdc9b7d7f09c347c1
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-07-25 15:34:08 +0100

    update

--- SmallerProjects/CurrencyConverter/currency.js
+++ SmallerProjects/CurrencyConverter/currency.js
@@ -4,10 +4,16 @@ document.addEventListener('DOMContentLoaded', function() {
         fetch('http://data.fixer.io/api/latest?access_key=56d56f08a108e812d9e0fc2c532d3d46')
         .then(response => response.json())
         .then(data => {
+            const amount = document.getElementById('number').value;
             const curr = document.getElementById('curDropdown').value;
-            const rate = data.rates[curr];
-            
-            document.querySelector('#results').innerHTML = `1 EUR is ${rate.toFixed(3)} ${curr}`
+            const rate = data.rates[curr] * amount;
+            if (amount.length == 0) {
+                document.querySelector('#results').innerHTML = "Please enter an amount of money to convert."
+            } else if (curr.length == 0) {
+                document.querySelector('#results').innerHTML = "Please select a valid currency."
+            } else {
+                document.querySelector('#results').innerHTML = `${amount} EUR is ${rate.toFixed(3)} ${curr}`
+            }
         });
 
         return false;
