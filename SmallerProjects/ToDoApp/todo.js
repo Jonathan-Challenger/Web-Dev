137d0886d79b8dd88008c12bb7c5de88f258fb06
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-07-28 15:18:32 +0100

    Added date and formatted

--- SmallerProjects/ToDoApp/todo.js
+++ SmallerProjects/ToDoApp/todo.js
@@ -1,8 +1,32 @@
+// Assigning variables
 const listbody = document.getElementById("list-box");
 const activity = document.getElementById("input-form");
 const listDiv = document.getElementById("list-box");
+const date = document.getElementById("date");
+
+// Finding and formatting the current Date
+n = new Date();
+day = n.getDay();
+dayNum = n.getDate();
+month = n.getMonth();
+
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+daysObj = {}
+for (let i = 1; i < 8; i++) {
+    daysObj[i] = days[i - 1];
+}
+
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+monthsObj = {}
+for (let i = 1; i < 13; i++) {
+    monthsObj[i] = months[i - 1];
+}
+
+// Adding date to relevant p tag
+date.innerText = `${daysObj[day]}, ${dayNum} ${monthsObj[month]}`;
 
 
+// Function that adds new task on each click
 function add() {
     if (activity.value === "") {
         alert("Please enter an activity to add.")
@@ -15,6 +39,7 @@ function add() {
     }
 }
 
+// Removes the task when cross is clicked
 listDiv.addEventListener("click", (e) => {
     if (e.target.tagName === "BUTTON") {
         const button = e.target;
