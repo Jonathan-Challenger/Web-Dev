57a0238bdcb7b4684878832661353c7b08cb9d63
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-07-29 15:08:39 +0100

    Added functionality to keep time and date

--- SmallerProjects/WeatherApp/weather.js
+++ SmallerProjects/WeatherApp/weather.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+const date = document.getElementById("clock");
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+function display_c() {
+    let refresh = 1000;
+    mytime = setTimeout('display_ct()', refresh);
+}
+function display_ct() {
+    let x = new Date();
+    day = x.getDay() - 1;
+    dayNum = x.getDate();
+    month = x.getMonth();
+    hours = x.getHours();
+    mins = x.getMinutes();
+    if( mins < 10) {
+        mins = "0"+mins;
+    }
+    date.innerText = `${days[day]}, ${dayNum} ${months[month]} ${hours}:${mins}`;
+    display_c();
+}
+
