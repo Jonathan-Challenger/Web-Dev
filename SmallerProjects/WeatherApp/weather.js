61b2d9e87dcd118edd4526d1979aff481922b938
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-02 14:17:04 +0100

    update1

--- SmallerProjects/WeatherApp/weather.js
+++ SmallerProjects/WeatherApp/weather.js
@@ -3,12 +3,11 @@ const date = document.getElementById("clock");
 const container = document.getElementsByClassName("container")[0];
 const add = document.getElementById("add-button");
 
-// Declaring days and months for time formatting
 days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 // Creating a function to display the date and time and update
-function display_c() {
+/* function display_c() {
     let refresh = 1000;
     mytime = setTimeout('display_ct()', refresh);
 }
@@ -24,7 +23,7 @@ function display_ct() {
     }
     date.innerText = `${days[day]}, ${dayNum} ${months[month]} ${hours}:${mins}`;
     display_c();
-}
+} */
 
 // Creating functionality for the add city button
 
@@ -36,7 +35,18 @@ add.addEventListener('click', (e) => {
     const url = `${url_base}/weather?q=${input.value}&appid=${api_key}&units=metric`;
 
     fetch(url).then(res => res.json()).then(data => {
-        const { main, name, sys, weather } = data;
+        const { main, name, sys, weather, timezone } = data;
+        /* const dif = timezone / 3600;
+        let d = new Date();
+        const day = d.getDay() - 1;
+        const dayNum = d.getDate();
+        const month = d.getMonth();
+        const hours = d.getHours() - 1 + dif;
+        let mins = d.getMinutes();
+        if (mins < 10) {
+            mins = '0' + mins;
+        } */
+        
 
         const myLi = document.createElement("li");
         myLi.classList.add("element");
@@ -62,19 +72,29 @@ add.addEventListener('click', (e) => {
     
 });
 
+function display_c() {
+    let refresh = 1000;
+    mytime = setTimeout('display_ct()', refresh);
+}
+function display_ct() {
+    const dif = timezone / 3600;
+    let d = new Date();
+    const day = d.getDay() - 1;
+    const dayNum = d.getDate();
+    const month = d.getMonth();
+    const hours = d.getHours() - 1 + dif;
+    let mins = d.getMinutes();
+    if (mins < 10) {
+        mins = '0' + mins;
+    }
+    myLi.innerHTML = `<span class="timezone">${days[day]}, ${dayNum} ${months[month]} ${hours}:${mins}</span>`;
+    display_c();
+}
+
+
 document.addEventListener("keyup", (e) => {
     if (e.keyCode === 13) {
         e.preventDefault();
         add.click();
     }
 });
-
-
-
-
-
-
-
-
-
-
