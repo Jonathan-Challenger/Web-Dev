b812b77d6efffc00f6cd48401c3c2c145ba0eaf4
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-03 12:32:41 +0100

    Adding Header component in container div

--- vue-projects/bookmark-app/src/App.vue
+++ vue-projects/bookmark-app/src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0px;
+  padding: 0px;
+}
+
+.container {
+  max-width: 60vw;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border-radius: 5px;
+  border: 1px solid black;
+  padding: 30px;
+  background-color: red;
 }
 </style>
