152dd1cce30c0765e3ac746040ac919308319b2c
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-03 16:51:26 +0100

    creating bookmarks to list all the bookmarks

---
+++ vue-projects/bookmark-app/src/components/Bookmarks.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="bookmark-container">
+        <div :key="bookmark.id" v-for="bookmark in bookmarks">
+            {{bookmark.name}}
+        </div>
+    </div>
+</template> 
+
+<script>
+// import Bookmark from './Bookmark.vue'
+
+
+export default {
+    name: 'Bookmarks',
+    props: {
+        bookmarks: Array,
+    },
+   /*  components: {
+        Bookmark,
+    } */
+}
+</script> 
