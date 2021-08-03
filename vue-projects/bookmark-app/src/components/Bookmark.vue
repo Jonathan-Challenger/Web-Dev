381a77bd2f27d9caf3100c9639ad7e76285b4605
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-03 16:51:10 +0100

    creating bookmark element to populate bookmarks

---
+++ vue-projects/bookmark-app/src/components/Bookmark.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="bookmark">
+        <h3>{{ bookmark.name }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Bookmark',
+    props: {
+        bookmark: Object,
+    },
+}
+</script>
