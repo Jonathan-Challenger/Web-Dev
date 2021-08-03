64d4ac2b1259f7ab2945d692f8f45ad8c41b24d4
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-03 12:38:26 +0100

    Update color styling for container and body

--- vue-projects/bookmark-app/src/App.vue
+++ vue-projects/bookmark-app/src/App.vue
@@ -22,14 +22,18 @@ export default {
   padding: 0px;
 }
 
+body {
+  background-color:#000814;
+}
+
 .container {
-  max-width: 60vw;
+  max-width: 50vw;
   margin: 30px auto;
   overflow: auto;
   min-height: 300px;
   border-radius: 5px;
-  border: 1px solid black;
+  border: 1px solid #ffc300;
   padding: 30px;
-  background-color: red;
+  background-color: #003566;
 }
 </style>
