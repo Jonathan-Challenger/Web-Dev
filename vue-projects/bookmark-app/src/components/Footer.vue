87d82a29ada6436d6aaaf64385db4adb8c5f393b
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-05 11:26:26 +0100

    create footer for bookmarker

---
+++ vue-projects/bookmark-app/src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <footer>
+        <h3>Made by Jonathan Challenger</h3>
+        <a target="_blank" href="https://github.com/Jonathan-Challenger/Web-Dev/tree/main/vue-projects/bookmark-app">Find the code here</a>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+}
+</script>
+
+<style scoped>
+
+footer {
+    position: absolute;
+    bottom: 0;
+    left: 23%;
+    display: flex;
+    justify-content: space-evenly;
+    margin: 5px;
+    width: 50vw;
+}
+
+h3 {
+    color: var(--txtcol);
+}
+
+a {
+    color: var(--txtcol);
+    text-decoration: none;
+}
+</style>
