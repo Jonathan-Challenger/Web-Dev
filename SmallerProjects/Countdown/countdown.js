e1149b0f96fe3c813af7dc136aaa6c9fc90d005a
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-06 23:50:15 +0100

    Adding jquery cdn to html and changing function in js to jquery

--- SmallerProjects/Countdown/countdown.js
+++ SmallerProjects/Countdown/countdown.js
@@ -1,3 +1,6 @@
+/* 
+THIS IS THE CODE IN PLAIN JAVASCRIPT WHICH PERFORMS THE SAME FUNCTION AS BELOW
+
 const mins = document.querySelectorAll('.input')[0];
 const secs = document.querySelectorAll('.input')[1];
 const start = document.getElementById('start-but');
@@ -9,5 +12,13 @@ start.addEventListener('click', () => {
     } else {
         display.innerHTML = `${mins.value}:${secs.value}`
     }
-})
+})  
+*/
 
+$("button#start-but").on('click', () => {
+    if ($("input.input")[0].value == '' && $("input.input")[1].value == '') {
+        alert("Please enter a time to count down from!")
+    } else {
+        $("h2#display").html(`${mins.value}:${secs.value}`)
+    }
+})
