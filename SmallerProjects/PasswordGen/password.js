f8c121c597af2a3a4c7a13c4dd1329f9dbc01847
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-06 11:21:59 +0100

    Adding length feature for user input

--- SmallerProjects/PasswordGen/password.js
+++ SmallerProjects/PasswordGen/password.js
@@ -1,15 +1,22 @@
 const gen = document.getElementById('gen-but');
 const output = document.getElementById('input-box');
 const msg = document.getElementsByClassName('hidden')[0];
+const len = document.getElementById('length-box');
 
 createPassword = () => {
     let res = ''
     let letts = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     let specials = '0123456789!@-_?%'
-    for (let i = 0; i < 6; i++) {
+    let letLens = Math.floor(len.value * 0.7)
+    let specLens = Math.ceil(len.value * 0.3)
+    if (len.value == '') {
+        alert("Please enter a length for your password")
+    }
+
+    for (let i = 0; i < letLens; i++) {
         res += letts.charAt(Math.floor(Math.random() * letts.length))
     }
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < specLens; i++) {
         res += specials.charAt(Math.floor(Math.random() * specials.length))
     }
     return res
