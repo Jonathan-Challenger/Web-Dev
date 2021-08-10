aab08e6c59d78025c65f1905d4d0b9bde5961f6f
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-10 01:21:17 +0100

    adding header to app

--- vue-projects/movie-app/src/App.vue
+++ vue-projects/movie-app/src/App.vue
@@ -1,26 +1,40 @@
 <template>
+  <Header />
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Secular+One&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Secular One', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: black;
 }
 </style>
