9c128d6badfbc52a6772d3dbe3a459a88e838925
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-11 21:06:52 +0100

    removing api key for privacy

--- vue-projects/movie-app/API_INFO.js
+++ vue-projects/movie-app/API_INFO.js
@@ -1,5 +1,4 @@
 const IMG_BASE = 'https://image.tmdb.org/t/p/w500';
-const API_KEY = '8b6426ed2bb28ce20b1467f0c9e172be'
 const DISCOVER = 'https://api.themoviedb.org/3/discover/movie?api_key=8b6426ed2bb28ce20b1467f0c9e172be&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_watch_monetization_types=flatrate'
 
 
