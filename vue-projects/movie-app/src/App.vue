a9d7ce1247071b2ea119d20b30191894836445ed
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-12 17:13:29 +0100

    removing data and methods from app to put into views pages

--- vue-projects/movie-app/src/App.vue
+++ vue-projects/movie-app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <Header @go-home="logoClick" @get-results="findResults"/>
-  <router-view :movies="movies" :shows="shows" :results="results"></router-view>
+  <router-view :results="results" ></router-view>
   <Footer />
 </template>
 
@@ -16,38 +16,16 @@ export default {
   },
   data() {
     return {
-      movies: [],
-      shows: [],
       results: [],
     }
   },
   methods: {
-    async getMovies() {
-      const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.VUE_APP_MOVIE_API}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_watch_monetization_types=flatrate`)
-
-      const data = await res.json()
-
-      return data.results
-    },
-    async getShows() {
-      const response = await fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.VUE_APP_MOVIE_API}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_watch_monetization_types=flatrate`)
-
-      const res = await response.json()
-
-      return res.results
-    },
     findResults(data) {
       this.results = data
-      // this.$router.push("/results")
     },
     async logoClick() {
       this.movies = await this.getMovies()
-      console.log(this.results)
-    }
-  },
-  async created() {
-    this.movies = await this.getMovies()
-    this.shows = await this.getShows()
+    },
   },
 }
 </script>
