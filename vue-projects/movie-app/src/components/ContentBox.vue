498191176d919290f5391b3e918cac1bc6298a87
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-12 12:20:10 +0100

    creating movie component

--- vue-projects/movie-app/src/components/ContentBox.vue
+++ vue-projects/movie-app/src/components/ContentBox.vue
@@ -1,42 +1,24 @@
 <template>
     <div class="content-container" v-if="movies.length > 0"> 
         <div v-for="movie in movies" :key="movie.id" class="movie">
-
-            <div v-if="movie.poster_path != null" class="image">
-                <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" alt="Poster">
-            </div>
-            <div v-else class="image">
-                <img id="mov-placeholder" src="../assets/movie-placeholder.png" alt="Poster">
-            </div>
-            <div class="content">
-                <h1>{{ movie.title }}</h1>
-                <div class="rating">
-                    <p>Rating:</p>
-                    <p>{{ movie.vote_average }}  <img id="star" src="../assets/star-solid.svg" alt="Star"></p>
-                </div>
-            </div>
-            <div class="overview">
-                <h1>{{ movie.title }}</h1>
-                <div class="rating-hover">
-                    <p>Rating: &nbsp;</p>
-                    <p>{{ movie.vote_average }}  <img id="star" src="../assets/star-solid.svg" alt="Star"></p>
-                </div>
-                <h3>Overview</h3>
-                {{ movie.overview }}
-            </div>
+            <Movie :movie="movie"/>
         </div>
     </div>
     <div class="no-response" v-else>
-            <h1>Sorry, we have no results...</h1>
+        <h1>Sorry, we have no results...</h1>
     </div>
 </template>
 
 <script>
+import Movie from './Movie'
+
 export default {
     name: 'ContentBox',
+    components: {
+        Movie,
+    },
     props: {
         movies: Array,
-        movie: Object,
     }
 }
 </script>
@@ -51,92 +33,6 @@ export default {
     gap: 4rem;
     padding: 1em;
     margin-bottom: 130px;
-}
-
-.movie {
-    background-color: white;
-    border-radius: 0 0 10px 10px;
-    cursor: pointer;
-    transition: transform 500ms ease;
-    max-width:285px;
-    position: relative;
-    overflow: hidden;
-}
-
-.movie:hover {
-    transform: scale(1.05);
-
-}
-
-.movie:hover .overview {
-    transform: translateY(0);
-}
-
-.content {
-    display: grid;
-    padding: 0 5px;
-    grid-template-columns: 3fr 1fr;
-    gap:1rem;
-    color: black;
-    height: 13%;
-}
-
-.rating {
-    display: flex;
-    flex-flow: column;
-}
-
-.image {
-    height:85%;
-}
-
-img {
-    height: 100%;
-    width: 100%;
-}
-
-#star {
-    height:17px;
-    width:15px;
-    padding-top: 6px;
-}
-
-h1 {
-    font-size: 18px;
-    text-align: left;
-}
-
-.overview {
-    color: black; 
-    display: flex;
-    flex-flow: column;  
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    padding: 1rem;
-    background-color: white; 
-    max-height: 100%;
-    transform: translateY(101%);
-    transition: transform 300ms ease;
-    font-size: 13px;
-}
-
-.overview h1 {
-    text-align: center;
-    font-size: 22px;
-}
-
-.overview h3 {
-    font-size: 20px;
-}
-
-.rating-hover {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-bottom: 15px;
-    font-size: 16px;
 }
 
 .no-response {
---
+++ vue-projects/movie-app/src/components/Movie.vue
@@ -0,0 +1,123 @@
+<template>
+    <div class="movie-container">
+        <div v-if="movie.poster_path != null" class="image">
+            <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" alt="Poster">
+        </div>
+        <div v-else class="image">
+            <img id="mov-placeholder" src="../assets/movie-placeholder.png" alt="Poster">
+        </div>
+        <div class="content">
+            <h1>{{ movie.title }}</h1>
+            <div class="rating">
+                <p>Rating:</p>
+                <p>{{ movie.vote_average }}  <img id="star" src="../assets/star-solid.svg" alt="Star"></p>
+            </div>
+        </div>
+        <div class="overview">
+            <h1>{{ movie.title }}</h1>
+            <div class="rating-hover">
+                <p>Rating: &nbsp;</p>
+                <p>{{ movie.vote_average }}  <img id="star" src="../assets/star-solid.svg" alt="Star"></p>
+            </div>
+            <h3>Overview</h3>
+            {{ movie.overview }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Movie',
+    props: {
+        movie: Object
+    },
+}
+</script>
+
+<style scoped>
+.movie-container {
+    background-color: white;
+    border-radius: 0 0 10px 10px;
+    cursor: pointer;
+    transition: transform 500ms ease;
+    max-width:285px;
+    position: relative;
+    overflow: hidden;
+    height: 500px;
+}
+
+.movie-container:hover {
+    transform: scale(1.05);
+}
+
+.movie-container:hover .overview {
+    transform: translateY(0);
+}
+
+.content {
+    display: grid;
+    padding: 0 5px;
+    grid-template-columns: 3fr 1fr;
+    gap:1rem;
+    color: black;
+    height: 13%;
+}
+
+.rating {
+    display: flex;
+    flex-flow: column;
+}
+
+.image {
+    height:85%;
+}
+
+img {
+    height: 100%;
+    width: 100%;
+}
+
+#star {
+    height:17px;
+    width:15px;
+    padding-top: 6px;
+}
+
+h1 {
+    font-size: 18px;
+    text-align: left;
+}
+
+.overview {
+    color: black; 
+    display: flex;
+    flex-flow: column;  
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    padding: 1rem;
+    background-color: white; 
+    max-height: 100%;
+    transform: translateY(101%);
+    transition: transform 300ms ease;
+    font-size: 13px;
+}
+
+.overview h1 {
+    text-align: center;
+    font-size: 22px;
+}
+
+.overview h3 {
+    font-size: 20px;
+}
+
+.rating-hover {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 15px;
+    font-size: 16px;
+}
+</style>
