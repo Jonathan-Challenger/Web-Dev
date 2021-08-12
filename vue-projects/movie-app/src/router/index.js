19b04712962735f3d7ba2113077749572d50cfc1
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-12 16:15:37 +0100

    updating router with views

--- vue-projects/movie-app/src/router/index.js
+++ vue-projects/movie-app/src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 import TV from '../views/TV'
+import Home from '../views/Home'
+import Results from '../views/Results'
 
 const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
     {
         path: '/tvshows',
         name: 'TV',
         component: TV
+    },
+    {
+        path: '/results',
+        name: 'Results',
+        component: Results
     }
 ]
 
