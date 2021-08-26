fef5a423d61985b1c3599b73e4d8459a6668c066
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-26 14:17:20 +0100

    removing img and changing home component

--- vue-projects/covid-tracker/src/App.vue
+++ vue-projects/covid-tracker/src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <Header />
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Home />
   <Footer />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Home,
     Header,
     Footer
   }
---
+++ vue-projects/covid-tracker/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Hello Home</h1>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<style scoped lang="scss">
+$primary: rgb(67, 102, 255);
+
+h1 {
+  font-size: 5rem;
+  color: $primary;
+}
+</style>
