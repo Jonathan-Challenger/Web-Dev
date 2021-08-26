931368c7f37a4c0cec98dafc361f336dd37d2e5f
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-26 11:20:19 +0100

    add header to App

--- vue-projects/covid-tracker/src/App.vue
+++ vue-projects/covid-tracker/src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <Header />
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 }
 </script>
