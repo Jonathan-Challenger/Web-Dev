fbe7da92c717beadb80ecb95983e354ef16b2e02
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-08-26 11:18:45 +0100

    add header component file

---
+++ vue-projects/covid-tracker/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav>
+        <h1>Header</h1>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+    color:red;
+}
+</style>
