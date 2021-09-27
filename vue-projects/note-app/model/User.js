0034c7e11afdf45b2b35b6f771e76c4557570514
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-27 14:44:18 +0100

    remove username from model

--- vue-projects/note-app/model/User.js
+++ vue-projects/note-app/model/User.js
@@ -6,10 +6,6 @@ const UserSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    username: {
-        type: String,
-        required: true
-    },
     email: {
         type: String,
         required: true
