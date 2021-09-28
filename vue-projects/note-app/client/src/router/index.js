5ba14aa7bd038246efcdc0ac764659e21c90d848
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-28 15:19:40 +0100

    add routes for links in router

--- vue-projects/note-app/client/src/router/index.js
+++ vue-projects/note-app/client/src/router/index.js
@@ -10,11 +10,23 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import('../views/Profile.vue')
+  },
 ]
 
 const router = createRouter({
