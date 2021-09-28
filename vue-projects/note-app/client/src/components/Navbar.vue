64830868180f5a96594c13a34e385014582a275e
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-28 12:39:33 +0100

    create navbar component

---
+++ vue-projects/note-app/client/src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-danger bg-danger">
+        <div class="container-fluid">
+            
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
