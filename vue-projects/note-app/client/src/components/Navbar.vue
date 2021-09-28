ecef2cfc8038f8d0cfc8529d325a052ee54a09e4
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-28 15:20:12 +0100

    create navbar with bootstrap

--- vue-projects/note-app/client/src/components/Navbar.vue
+++ vue-projects/note-app/client/src/components/Navbar.vue
@@ -1,8 +1,33 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-danger bg-danger">
-        <div class="container-fluid">
-            
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">uNote</router-link> 
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <router-link class="nav-link" to="/">Home</router-link> 
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" to="/about">About</router-link> 
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" to="/register">Register</router-link> 
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" to="/login">Login</router-link> 
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" to="/profile">Profile</router-link> 
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" to="/logout">Logout</a> 
+            </li>
+        </ul>
         </div>
+    </div>
     </nav>
 </template>
 
