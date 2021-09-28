9a5aedf7cd5232e4e246a0ceca0173ef16182a7b
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-28 15:19:58 +0100

    remove router links from appjs

--- vue-projects/note-app/client/src/App.vue
+++ vue-projects/note-app/client/src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <Navbar />
-  <!-- <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="nav">
     <router-view/>
-  </div> -->
+  </div>
 </template>
 
 <script>
