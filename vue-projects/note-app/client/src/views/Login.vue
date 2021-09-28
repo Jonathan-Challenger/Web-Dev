6b803e06927101d9f4c4e82baf053a01d75f7554
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-28 15:41:06 +0100

    start creating login page

--- vue-projects/note-app/client/src/views/Login.vue
+++ vue-projects/note-app/client/src/views/Login.vue
@@ -1,11 +1,30 @@
 <template>
     <div>
-        <h2>Login Page</h2>
+        <h2 class="text-center my-5">Login</h2>
+        <div class="row">
+            <div class="card mx-auto px-0">
+                <div class="card-header text-white bg-primary">
+                    <h4>Login</h4>
+                </div>
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" placeholder="Name" name="name" v-model="name" class="form-control">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'Login'
-}
+
 </script>
+
+<style>
+.card {
+    width: 60%;
+}
+</style>
