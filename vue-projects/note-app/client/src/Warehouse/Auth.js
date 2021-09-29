f61fb46743ce3c1d5e4a295875d7782760fe9249
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-29 11:07:29 +0100

    create warehouse skeleton

---
+++ vue-projects/note-app/client/src/Warehouse/Auth.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import router from '../router';
+
+const state = {
+
+};
+
+const getters = {
+
+};
+
+const actions = {
+
+};
+
+const mutations = {
+
+};
+
+export default {
+    state,
+    actions,
+    getters,
+    mutations
+}
