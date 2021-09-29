9e8dae6b552120533e06701bc3c84cee83864e0d
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-29 12:55:27 +0100

    fix errors in mainjs

--- vue-projects/note-app/client/src/main.js
+++ vue-projects/note-app/client/src/main.js
@@ -4,12 +4,14 @@ import router from './router'
 import store from './store'
 import axios from 'axios';
 
-const app = createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App).use(store).use(router)
 
 // Load token from localStorage
-app.config.globalProperties = axios;
+app.config.globalProperties.$http = axios;
 const token = localStorage.getItem("token");
 // If token then append default axios authorisation headers
 if (token) {
     app.config.globalProperties.$http.defaults.headers.common['Authorization'] = token;
 }
+
+app.mount('#app');
