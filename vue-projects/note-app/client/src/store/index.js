44b5e87c5cd994d41dfd0c29275eedb950b11bdc
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-29 11:13:58 +0100

    add Auth to modules in store

--- vue-projects/note-app/client/src/store/index.js
+++ vue-projects/note-app/client/src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import Auth from '../Warehouse/Auth';
 
 export default createStore({
   state: {
@@ -8,5 +9,6 @@ export default createStore({
   actions: {
   },
   modules: {
+    Auth
   }
 })
