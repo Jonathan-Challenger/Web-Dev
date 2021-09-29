c755645e4e0f19352ebbf4986f3f1381888873ce
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-29 11:54:29 +0100

    add base state and loggedin getters

--- vue-projects/note-app/client/src/Warehouse/Auth.js
+++ vue-projects/note-app/client/src/Warehouse/Auth.js
@@ -1,12 +1,16 @@
-import axios from 'axios';
-import router from '../router';
+// import axios from 'axios';
+// import router from '../router';
 
 const state = {
-
+    token: localStorage.getItem('token') || '',
+    user: {},
+    status: ''
 };
 
 const getters = {
-
+    isLoggedIn: state => !!state.token,
+    authState: state => state.status,
+    user: state => state.user
 };
 
 const actions = {
