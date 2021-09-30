9cfbdc1691371579d7f91db4b06b715e89babb41
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-09-30 15:57:56 +0100

    create logoutCheck component to possibly use

---
+++ vue-projects/note-app/client/src/components/LogoutCheck.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container bg-dark w-25 fixed-top">
+        <h4 class="text-white text-center pt-5">Are you sure?</h4>
+        <div class="d-flex justify-content-center">
+            <button type="button" class="btn btn-lg btn-primary mt-5 mx-4">Yes</button>
+            <button type="button" class="btn btn-lg btn-primary mt-5 mx-4">No</button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'LogoutCheck',
+}
+</script>
+
+<style>
+.container {
+    height: 250px;
+    border-radius: 5px;
+}
+</style>
