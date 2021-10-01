52660395788094432b62d4fd468e62afac96298c
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-01 10:11:44 +0100

    add methods to emmit up to app

--- vue-projects/note-app/client/src/components/LogoutCheck.vue
+++ vue-projects/note-app/client/src/components/LogoutCheck.vue
@@ -2,8 +2,8 @@
     <div class="container bg-dark w-25 fixed-top">
         <h4 class="text-white text-center pt-5">Are you sure?</h4>
         <div class="d-flex justify-content-center">
-            <button type="button" class="btn btn-lg btn-primary mt-5 mx-4">Yes</button>
-            <button type="button" class="btn btn-lg btn-primary mt-5 mx-4">No</button>
+            <button @click="$emit('logoutUser')" type="button" class="btn btn-lg btn-primary mt-5 mx-4">Yes</button>
+            <button @click="$emit('cancelLogout')" type="button" class="btn btn-lg btn-primary mt-5 mx-4">No</button>
         </div>
     </div>
 </template>
