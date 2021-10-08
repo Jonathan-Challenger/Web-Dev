9adaaa3ecc6e3ed7a31d986edc8a0fd7051f9f12
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-08 15:20:23 +0100

    Update welcome.php

--- php_projects/login-system/welcome.php
+++ php_projects/login-system/welcome.php
@@ -1,5 +1,23 @@
+<?php require_once 'connection.php';
+
+    session_start();
+
+    if (!isset($_SESSION['user'])) {
+        header("location: index.php");
+    }
+
+?>
+
 <?php include_once 'header.php' ?>
 
     <h2 class="text-center">Welcome to the homepage</h2>
+
+    <?php 
+    
+        echo "<h3 class='text-center'> User: ".$_SESSION['user']['name']."</h3>";
+
+    ?>
+
+    <a class="btn btn-primary" href="logout.php">Logout</a>
 
 <?php include_once 'footer.php' ?>
