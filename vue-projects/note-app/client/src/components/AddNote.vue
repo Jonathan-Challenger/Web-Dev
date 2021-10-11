c0262582e1c23d7cfd1f3fc3e157f27f1ad4d876
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-11 14:48:57 +0100

    create add note component with form

---
+++ vue-projects/note-app/client/src/components/AddNote.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container mt-4" style="width: 800px">
+        <h3>Add Note</h3>
+        <form>
+            <div class="form-group">
+                <input type="text" placeholder="Title" name="title" v-model="title" class="form-control">
+            </div>
+            <div class="form-group my-3">
+                <textarea class="form-control" aria-label="With textarea" rows="10" style="resize:none;"></textarea>
+            </div>
+            <div class="form-group d-flex justify-content-center">
+                <input type="submit" class="btn btn-primary mx-3" value="Save" style="width:150px"/>
+                <input type="submit" class="btn btn-danger mx-3" value="Cancel" style="width:150px"/>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AddNote'
+}
+</script>
+
+<style>
+
+</style>
