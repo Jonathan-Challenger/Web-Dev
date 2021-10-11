62e8be9e4bd38cc736d2b3eda59e338282b5466f
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-11 14:49:31 +0100

    create db schema for a note

---
+++ vue-projects/note-app/model/Note.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+// User Model
+const NoteSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        default: 'public',
+        enum: ['public', 'private']
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Note = mongoose.model('Note', NoteSchema);
+
+module.exports = Note;
