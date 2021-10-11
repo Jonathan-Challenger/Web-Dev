246a2e8005eb26da1b08526bdff95f4ad5f9185f
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-11 14:15:17 +0100

    add methods and computed to get user profile data and add to profile view

--- vue-projects/note-app/client/src/views/Profile.vue
+++ vue-projects/note-app/client/src/views/Profile.vue
@@ -1,5 +1,25 @@
 <template>
     <div>
         <h2 class="text-center my-4">Profile Page</h2>
+        <div class="card">
+            <ul class="list-group">
+                <li class="list-group-item">Name: {{ user.name }}</li>
+                <li class="list-group-item">Email: {{ user.email }}</li>
+            </ul>
+        </div>
     </div>
 </template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+    computed: mapGetters(['user']),
+    methods: {
+        ...mapActions(['getProfile'])
+    },
+    created() {
+        this.getProfile()
+    }
+}
+</script>
