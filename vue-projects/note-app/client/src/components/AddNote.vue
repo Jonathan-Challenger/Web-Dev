3965ac71eed5b12cd57dcb4296ec891b4b8b7b48
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-11 14:58:45 +0100

    add input for private or public note

--- vue-projects/note-app/client/src/components/AddNote.vue
+++ vue-projects/note-app/client/src/components/AddNote.vue
@@ -8,6 +8,13 @@
             <div class="form-group my-3">
                 <textarea class="form-control" aria-label="With textarea" rows="10" style="resize:none;"></textarea>
             </div>
+            <div class="input-field">
+                <label for="status" class="px-3">Status</label>
+                <select id="status" name="status">
+                    <option value="public" selected>Public</option>
+                    <option value="private">Private</option>
+                </select>
+            </div>
             <div class="form-group d-flex justify-content-center">
                 <input type="submit" class="btn btn-primary mx-3" value="Save" style="width:150px"/>
                 <input type="submit" class="btn btn-danger mx-3" value="Cancel" style="width:150px"/>
