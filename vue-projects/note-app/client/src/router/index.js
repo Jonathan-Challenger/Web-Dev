da0fcbd982efed426d861cfacd035fefc0793708
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-11 12:17:41 +0100

    add route protection

--- vue-projects/note-app/client/src/router/index.js
+++ vue-projects/note-app/client/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-// import index from '../store/index'
+import index from '../store/index'
 
 const routes = [
   {
@@ -16,23 +16,52 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: () => import('../views/Login.vue')
+    component: () => import('../views/Login.vue'),
+    meta: {
+      requiresGuest: true
+    }
   },
   {
     path: '/register',
     name: 'register',
-    component: () => import('../views/Register.vue')
+    component: () => import('../views/Register.vue'),
+    meta: {
+      requiresGuest: true
+    }
   },
   {
     path: '/profile',
     name: 'profile',
-    component: () => import('../views/Profile.vue')
+    component: () => import('../views/Profile.vue'),
+    meta: {
+      requiresAuth: true
+    }
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!index.getters.isLoggedIn) {
+      // User not logged in so redirect to login page
+      next('/login');
+    } else {
+      next();
+    }
+  } else if (to.matched.some(record => record.meta.requiresGuest)) {
+    if (index.getters.isLoggedIn) {
+      // User not logged in so redirect to login page
+      next('/profile');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 })
 
 export default router
