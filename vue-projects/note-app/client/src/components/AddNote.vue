7a481d2c2e678cfb246c4be2dc5a6d8df02ee3e6
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-12 15:42:39 +0100

    add frontend functionality to click and create note in db

--- vue-projects/note-app/client/src/components/AddNote.vue
+++ vue-projects/note-app/client/src/components/AddNote.vue
@@ -6,17 +6,17 @@
                 <input type="text" placeholder="Title" name="title" v-model="title" class="form-control">
             </div>
             <div class="form-group my-3">
-                <textarea class="form-control" aria-label="With textarea" rows="10" style="resize:none;"></textarea>
+                <textarea class="form-control" aria-label="With textarea" rows="10" style="resize:none;" v-model="body"></textarea>
             </div>
             <div class="input-field">
                 <label for="status" class="px-3">Status</label>
-                <select id="status" name="status">
+                <select id="status" name="status" v-model="status">
                     <option value="public" selected>Public</option>
                     <option value="private">Private</option>
                 </select>
             </div>
             <div class="form-group d-flex justify-content-center">
-                <input type="submit" class="btn btn-primary mx-3" value="Save" style="width:150px"/>
+                <input @click.prevent="testPost()" type="submit" class="btn btn-primary mx-3" value="Save" style="width:150px"/>
                 <input type="submit" class="btn btn-danger mx-3" value="Cancel" style="width:150px"/>
             </div>
         </form>
@@ -24,8 +24,31 @@
 </template>
 
 <script>
+import axios from 'axios';
+import { mapActions } from 'vuex'
+
 export default {
-    name: 'AddNote'
+    name: 'AddNote',
+    data() {
+        return {
+            title: "",
+            body: "",
+            status: "",
+        }
+    },
+    methods: {
+        ...mapActions(['getProfile']),
+        async testPost() {
+            let note = {
+                title: this.title,
+                body: this.body,
+                status: this.status
+            }
+            let res = await axios.post("http://localhost:5000/api/notes", note);
+            return res.data;
+
+        }
+    }
 }
 </script>
 
