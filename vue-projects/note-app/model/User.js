400ca25f3f78bb57edca2a4e914b45e9c4dd9fe9
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-10-12 15:42:44 +0100

    Update User.js

--- vue-projects/note-app/model/User.js
+++ vue-projects/note-app/model/User.js
@@ -14,6 +14,10 @@ const UserSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
     date: {
         type: Date,
         default: Date.now
